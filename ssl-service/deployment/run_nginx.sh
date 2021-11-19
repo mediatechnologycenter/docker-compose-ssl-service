ba6749b061c9e98e696e480bed35c5dd7f027e1c
marcwi <marc.willhaus@inf.ethz.ch>
2021-11-19 14:27:36 +0100

    debugging nginx file problem

--- ssl-service/deployment/run_nginx.sh
+++ ssl-service/deployment/run_nginx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 export DOLLAR='$'
-envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/templates/nginx.conf.template
+envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
