dfb2d03f6e9151948fb5a7d9d947da65096e9fbf
thomaste <thomas.steinmann@inf.ethz.ch>
2021-12-07 08:32:57 +0000

    Feature/disable

---
+++ certbot/run_certbot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SSL_ENABLED=$(echo "$SSL_ENABLED" | awk '{print tolower($0)}')
+
+echo "ssl_enabled: $SSL_ENABLED"
+echo "BASE_URL: $BASE_URL"
+echo "SSL_EMAIL: $SSL_EMAIL"
+
+if [ "$SSL_ENABLED" == "true" ]; then
+  certbot certonly -n --standalone --agree-tos --email "${SSL_EMAIL}" -d "${BASE_URL}" -d "www.${BASE_URL}"
+
+else
+  echo "SSL is disabled, skipping certbot call"
+
+fi
+
+# Keep alive
+tail -f /dev/null
