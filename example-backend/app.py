cd464c18698b7bdbcba3761106bdc530acee1f7c
masus <thomas_st@hotmail.com>
2022-01-26 16:41:41 +0100

    Removed config

--- example-backend/app.py
+++ example-backend/app.py
@@ -1,6 +1,5 @@
 from mtc_api_utils.api import BaseApi
-from config import ExampleConfig
 
 app = BaseApi(__name__, is_ready=lambda: True, cors_enabled=True)
 
-print("API is listening to {}".format(ExampleConfig.backend_url))
+print("Proxy is running")
